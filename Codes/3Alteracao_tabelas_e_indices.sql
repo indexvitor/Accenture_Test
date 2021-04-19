1fe86e06cd7b610af30dd8cddfa2885552f3bf35
indexvitor <82410838+indexvitor@users.noreply.github.com>
2021-04-19 11:16:51 -0300

    Add files via upload

--- Codes/3Alteracao_tabelas_e_indices.sql
+++ Codes/3Alteracao_tabelas_e_indices.sql
@@ -4,28 +4,35 @@
 	ADD DataUltimoFluxo DATE NULL
 	GO
 
-/* ADICIONANDO COLUNA IDCRONOGRAMA */
+/* identificador de fluxo do cronograma do contrato */ --checksum
 ALTER TABLE tbCronograma
-	add IDCronograma INT IDENTITY(1,1) NOT NULL
+	ADD IDFluxo AS CHECKSUM(IDContrato,IDTranche,Tipo,DatBase)
 GO
 
-/* identificador de fluxo do cronograma do contrato */ 
-ALTER TABLE tbCronograma
-	ADD IDFluxo AS CONCAT(IDContrato,IDTranche,Tipo,convert(varchar, "DatBase", 112),IdCronograma)
-GO
+		--ALTER TABLE tbCronograma
+		--	ADD IDFluxo AS CONCAT(IDContrato,IDTranche,Tipo,convert(varchar, "DatBase", 112),IdCronograma)
+		--GO
 
--- DEFINIÇÃO DO CAMPO COMO PK
-ALTER TABLE tbCronograma
-	ADD CONSTRAINT PK_IDFluxo PRIMARY KEY (IDFluxo)
-GO
+--https://docs.microsoft.com/pt-br/sql/t-sql/functions/checksum-transact-sql?view=sql-server-ver15
+--https://stackoverflow.com/questions/45468766/checksum-example-explanation-in-sql-server-t-sql
+--https://www.mssqltips.com/sqlservertip/1023/checksum-functions-in-sql-server-2005/
+
+/*----------------------------------------------------------------*/
 
 -- ADICIONAMENTO DE NONCLUSTER INDEX
-CREATE NONCLUSTERED INDEX TbCronograma
-ON tbCronograma (IDContrato ASC, DataFoto DESC)
+CREATE NONCLUSTERED INDEX NIX_TbCronograma
+	ON tbCronograma (IDContrato ASC, DataFoto DESC)
+	INCLUDE(IDFLUXO, TIPO, DATBASE, DATABAIXA, DATAEVENTO, 
+		   FOIREALIZADO, PROJETADO, TaxaCambio, TaxaVariante, Taxa)
 GO
 
 CREATE NONCLUSTERED INDEX NIX_TbContrato
-ON tbContrato (CodContrato ASC, Emissao DESC, Vencimento DESC)
+	ON tbContrato (CodContrato ASC, Emissao DESC, Vencimento DESC)
+	Include (NomeContrato, tipoinstrumento, Moeda, Indexador, 
+			 Calendario, Basis, Pais, SeriePreco, DataUltimoFluxo)
 GO
 
-select * from [dbo].[tbCronograma]
+--https://stackoverflow.com/questions/1307990/why-use-the-include-clause-when-creating-an-index
+
+
+
